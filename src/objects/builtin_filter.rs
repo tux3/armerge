180564c6e0b8b025c19cbfd28e3178f2e5b5f330
Javier Blazquez <jblazquez@riotgames.com>
2024-12-21 13:54:23 -0800

    Add support for ordering object files during merge

--- src/objects/builtin_filter.rs
+++ src/objects/builtin_filter.rs
@@ -9,7 +9,7 @@ use crate::objects::syms::ObjectSyms;
 pub fn merge_required_objects(
     _obj_dir: &Path,
     merged_path: &Path,
-    objects: &HashMap<PathBuf, ObjectSyms>,
+    objects: &[PathBuf],
     keep_or_remove: ArmergeKeepOrRemove,
     regexes: &[Regex],
 ) -> Result<(), MergeError> {
@@ -18,7 +18,7 @@ pub fn merge_required_objects(
     }
 
     // The merging part is still not builtin, it has to be done by a real linker
-    merge::create_merged_object(merged_path, &[], objects.keys(), false)?;
+    merge::create_merged_object(merged_path, &[], objects, false)?;
 
     // Filtering the symbols is faster in pure Rust, compared to calling the system's objcopy
     let merged_elf = std::fs::read(merged_path)?;
